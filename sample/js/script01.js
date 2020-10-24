8dbc6e1ba8f3d6c53ac02502047c6afe30d15392
Mooponrawit <68629338+Mooponrawit@users.noreply.github.com>
2020-10-25 00:18:27 +0900

    js

--- sample/js/script01.js
+++ sample/js/script01.js
@@ -1,6 +1,48 @@
 $(function(){
+ 
+    let txt1 = "おはようございます！";
+    let txt2 = "おげんきですか？";
+    let num1 = 20;
+    let num2 = 32;
 
+    function seyHello(){
+      $("#content").text("こんにちは！");
+    }
 
+    function saySomething(txt){
+      $("#content").text(txt);
+    }
 
+    function samTwo(t1,t2){
+        $("#content").text(t1+t2);
+    }
+    　　
+    $("#button01").on("click",function(){
+      seyHello();
+    })
+    
 
-})
+    $("#button02").on("click",function(){
+      saySomething(txt1)
+    })
+    
+    $("#button03").on("click",function(){
+      //$("#content").text(txt1 + txt2);
+      samTwo(txt1,txt2);
+    })
+    
+    $("#button04").on("click",function(){
+        //$("#content").text(num1 + num2);
+        samTwo(num1,num2);
+    })
+    
+    // + - * /
+    
+    $("#button05").on("click",function(){
+        samTwo(txt1,txt2);
+        samTwo(num1,num2);
+    })
+    
+    
+   })
+   
